3be255b9c235bcf443a7d816d055cffeb2183397
Fernando Falci <fernando.falci@charter.com>
2016-06-30 11:44:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# as-json
+
+Returns the stdin in a formatted JSON
+
+## Install
+```npm i -g as-json```
+
+## Use
+```wget -O - -o /dev/null https://raw.githubusercontent.com/Falci/as-json/master/not-formatted-example.json | as-json```
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let raw = '';
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  let chunk = process.stdin.read();
+
+  if(chunk === null) return;
+
+  raw += chunk;
+
+});
+
+process.stdin.on('end', function() {
+  let json = JSON.parse(raw),
+    formatted = JSON.stringify(json, null, 4);
+
+  process.stdout.write(formatted);
+  process.stdout.write('\n');
+});
